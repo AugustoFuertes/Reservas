64d6c51a9a20186272b778b68c31a251bd7c0161
Augusto <augustofuertes11@gmail.com>
2023-04-17 18:51:02 -0300

    Third commit

--- app/src/main/java/com/example/reservas/activities/ReservasActivity.java
+++ app/src/main/java/com/example/reservas/activities/ReservasActivity.java
@@ -82,14 +82,14 @@ public class ReservasActivity extends AppCompatActivity {
     public void init(){
 
         reservas= new ArrayList<>();
-        reservas.add(new Reserva("Fausto", "10:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Pedro", "11:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Fran", "12:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Lucho", "13:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Agus", "14:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Fausto", "10:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Pedro", "11:30", fechaActual, "cancha 1", "pago seña"));
-        reservas.add(new Reserva("Fran", "12:30", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Fausto", "10:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Pedro", "11:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Fran", "12:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Lucho", "13:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Agus", "14:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Fausto", "10:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Pedro", "11:00", fechaActual, "cancha 1", "pago seña"));
+        reservas.add(new Reserva("Fran", "12:00", fechaActual, "cancha 1", "pago seña"));
         reservas.add(new Reserva("Lucho", "13:30", fechaActual, "cancha 1", "pago seña"));
         reservas.add(new Reserva("Agus", "14:30", fechaActual, "cancha 1", "pago seña"));
         reservas.add(new Reserva("Fausto", "10:30", fechaActual, "cancha 1", "pago seña"));
